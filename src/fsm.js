60abdf4a4ba1092603988712d90253598503753f
Fedor <yakubovich.fedor@yandex.by>
2017-02-05 18:42:12 +0300

    Done

--- src/fsm.js
+++ src/fsm.js
@@ -1,59 +1,121 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
+  /**
+   * Creates new FSM instance.
+   * @param config
+   */
+  constructor(config) {
+    if (!config) {
+      throw new Error('Config does not exist!')
+    }
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+    this.initial = config.initial
+    this.state = config.initial
+    this.states = config.states
+    this.undoHistory = []
+    this.redoHistory = []
+  }
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+  /**
+   * Returns active state.
+   * @returns {String}
+   */
+  getState() {
+    return this.state
+  }
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
+  /**
+   * Goes to specified state.
+   * @param state
+   */
+  changeState(state) {
+    if (!this.states[state]) {
+      throw new Error('State with that name does not exist')
+    }
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+    this.redoHistory = []
+    this.undoHistory.push(this.state)
+    this.state = state
+  }
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+  /**
+   * Changes state according to event transition rules.
+   * @param event
+   */
+  trigger(event) {
+    if (!this.states[this.state].transitions[event]) {
+      throw new Error('That event in the state does not exist')
+    }
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
+    this.redoHistory = []
+    this.undoHistory.push(this.state)
+    this.state = this.states[this.state].transitions[event]
+  }
 
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
+  /**
+   * Resets FSM state to initial.
+   */
+  reset() {
+    this.state = this.initial
+  }
 
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+  /**
+   * Returns an array of states for which there are specified event transition rules.
+   * Returns all states if argument is undefined.
+   * @param event
+   * @returns {Array}
+   */
+  getStates(event) {
+    let states = []
+
+    if (!event) {
+      return Object.keys(this.states)
+    }
+
+    for (let item in this.states) {
+      if (this.states[item].transitions[event]) {
+        states.push(item)
+      }
+    }
+
+    return states
+  }
+
+  /**
+   * Goes back to previous state.
+   * Returns false if undo is not available.
+   * @returns {Boolean}
+   */
+  undo() {
+    if (this.undoHistory.length == 0) {
+      return false
+    }
+
+    this.redoHistory.push(this.state)
+    this.state = this.undoHistory.pop()
+    return true
+  }
+
+  /**
+   * Goes redo to state.
+   * Returns false if redo is not available.
+   * @returns {Boolean}
+   */
+  redo() {
+    if (this.redoHistory == 0) {
+      return false
+    }
+
+    this.state = this.redoHistory.pop()
+    return true
+  }
+
+  /**
+   * Clears transition history
+   */
+  clearHistory() {
+    this.undoHistory = []
+    this.redoHistory = []
+  }
 }
 
 module.exports = FSM;
